4ab5a4ecdfb65c06eb658b42da6d5d86ae8d2441
itay2108 <itay2108@gmail.com>
2024-11-21 21:59:58 +0200

    Initial Commit

---
+++ Encrypted Recipes/Encrypted_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Encrypted_RecipesApp.swift
+//  Encrypted Recipes
+//
+//  Created by Itay Gervash on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Encrypted_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
